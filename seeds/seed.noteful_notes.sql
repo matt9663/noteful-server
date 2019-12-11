f0e72a2f5937c3fa4de592b97f03c9a62309977a
Matt Wood <matt9663@gmail.com>
2019-12-11 09:49:14 +0000

    first heroku commit

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO noteful_notes (note_name, content, folder_id)
+  VALUES
+    ('Dogs', 'Dogs are so great. Definitely Top 3 animals', 1),
+    ('Cats', 'Cats are also great, definitely Top 3 as well', 1),
+    ('Nuthin', 'Ad non nulla nisi aute magna commodo nulla laboris sunt.', 2),
+    ('More nuthin', 'Eiusmod laborum commodo do cupidatat dolor ad.', 2),
+    ('Cool bands', 'Vulfpeck, The Beatles, James Brown, UMO', 3),
+    ('Cool video games', 'Civ 6, Legend of Zelda, Witcher 3', 3);
